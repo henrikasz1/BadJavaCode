bdcb39d5e8231197683c18d720fcd115c2bfd4a5
henrikasz1 <58070388+henrikasz1@users.noreply.github.com>
2022-10-25 23:15:53 +0300

    Update Main.java

--- src/jv/learn/Main.java
+++ src/jv/learn/Main.java
@@ -1,8 +1,56 @@
 package jv.learn;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Hello, World!");
+    }
+}
+
+public class User {
+
+    private  int​ id​ ;
+    private​ String firstName​;
+    private​ String lastName​;
+    private​ int age;
+    private String profession;
+    private ArrayList<User> children;
+
+    public User(int id, String firstName, String lastName, int age) {
+
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.profession = profession;
+
+    }
+
+    public String getChildName(User child) {
+        if (!this.children.contains(child)) {
+            new Exception("Invalid argument!");
+        } else {
+            String name = null;
+            if (child.getFirstName() != null) {
+                name = child.getFirstName();
+            }
+            if (name == "Harry") {
+                name.replace('r', 'p');
+            }
+            if (name != null || name.length() > 0) {
+                name.concat(child.getLastName());
+            }
+        }
+        return this.getChildName(child);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
     }
 }
